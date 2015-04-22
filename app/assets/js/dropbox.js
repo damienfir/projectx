43a6fbad7990ce94b9426384ba3d3b93c8c57429
Damien Firmenich <fir.damien@gmail.com>
2015-04-22 18:13:12 +0200

    feedback working

--- app/assets/js/dropbox.js
+++ app/assets/js/dropbox.js
@@ -2,32 +2,35 @@ define([
   "backend",
   "dropbox-api",
   "ui",
-  "mosaic"
-], function(backend, Dropbox, ui, mosaic){
+  "mosaic",
+  "observers"
+], function(backend, Dropbox, ui, mosaic, observers){
 
   Dropbox.appKey = "z7bsczzlaqyb8zr";
 
   var DropboxModule = function() {
     var self = this;
+    self.watch = new observers(["uploading","progress","processing","loaded","failed","submitted"]);
 
     self.upload = function() {
+      self.watch.notify("submitted");
       Dropbox.choose({
         success: function(files) {
-          ui.uploading(1);
+          self.watch.notify("uploading", [files.length]);
 
           backend.reset()
           .then(function(){
-            ui.notify(100,0);
+            self.watch.notify("progress", [100, 0]);
             return backend.uploadFromDropbox(JSON.stringify(files));
           })
           .then(function() {
-            ui.processing();
+            self.watch.notify("processing");
             return backend.process();
           })
           .then(function(res) {
             var obj = JSON.parse(res);
+            self.watch.notify("loaded", [obj]);
             mosaic.loaded(obj);
-            ui.loaded(obj.mosaic);
           })
           .fail(function(error){
             console.log(error);
