ba5b145d4331adade396689a2595437e28644fec
Damien Firmenich <fir.damien@gmail.com>
2015-05-26 17:47:18 +0200

    moving to angular

--- app/assets/js/main.js
+++ app/assets/js/main.js
@@ -1,6 +1,6 @@
 window.GoogleAnalyticsObject = "__ga__";
 window.__ga__ = {
-    q: [["create", "UA-69644-18", "none"]],
+    q: [["create", "UA-69644-18"]],
     l: Date.now()
 };
 
@@ -16,7 +16,12 @@ require.config({
     },
     "ga": {
       exports: "__ga__"
-    }
+    },
+    "angular": {
+      exports: "angular"
+    },
+    "angular-resource": ["angular"],
+    "angular-cookies": ["angular"]
   },
   paths: {
     'q': "../bower_components/q/q",
@@ -28,13 +33,16 @@ require.config({
     'pinterest-api': "//assets.pinterest.com/js/pinit",
     "ga": "//www.google-analytics.com/analytics",
     "skrollr": "../bower_components/skrollr/dist/skrollr.min",
-    "skrollr-menu": "../bower_components/skrollr-menu/dist/skrollr.menu.min"
+    "skrollr-menu": "../bower_components/skrollr-menu/dist/skrollr.menu.min",
+    "angular": "../bower_components/angular/angular.min",
+    "angular-resource": "../bower_components/angular-resource/angular-resource.min",
+    "angular-cookies": "../bower_components/angular-cookies/angular-cookies.min"
   }
 });
 
 
 require(['ga', 'jquery', 'bootstrap'], function(ga, $, _) {
-  ga('send', 'pageview');
+  // ga('send', 'pageview');
 
   // Google Analytics triggers
   $(".modal").on("show.bs.modal", function(ev){ ga("send", "pageview", $(ev.target).data("content")); });
